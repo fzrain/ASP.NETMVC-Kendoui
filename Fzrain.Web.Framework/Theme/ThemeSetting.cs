7d94090ac29cb7e7572a607560175995c4fcbb39
fzrain <250970574@qq.com>
2014-12-18 17:28:11 +0800

    新增换肤功能

---
+++ Fzrain.Web.Framework/Theme/ThemeSetting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fzrain.Web.Framework.Theme
+{
+    public  class ThemeSetting
+    {
+        /// <summary>
+        /// 得到当前主题
+        /// </summary>
+        public static string CurrentTheme
+        {
+            get
+            {
+                var cookie = System.Web.HttpContext.Current.Request.Cookies["theme"];
+                return cookie != null && !string.IsNullOrWhiteSpace(cookie.Value) ? cookie.Value : "default";
+            }
+        }
+
+        public static void SetTheme(string theme)
+        {
+        }
+    }
+}
