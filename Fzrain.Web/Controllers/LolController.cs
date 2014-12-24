29e591f03e6cbec55e13b4fffd6fbe05d404bbfe
fzrain <250970574@qq.com>
2014-12-24 23:13:33 +0800

    优化

--- Fzrain.Web/Controllers/LolController.cs
+++ Fzrain.Web/Controllers/LolController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Fzrain.Core.Domain;
+using Fzrain.Core.Domain.Lol;
 using Fzrain.Service;
 using Fzrain.Service.Lol;
 using Fzrain.Web.Models.Lol;
@@ -17,11 +18,12 @@ namespace Fzrain.Web.Controllers
     public class LolController : Controller
     {
         private readonly ILolService lolService;
-    
+        public List<string> MyHeroList = new List<string> { "笨笨秒杀上帝", "网络中断突然" };
+
         public LolController(ILolService lolService, IUserService userService)
         {
             this.lolService = lolService;
-      
+
         }
         // GET: Lol
         public ActionResult Index()
@@ -34,39 +36,39 @@ namespace Fzrain.Web.Controllers
             if (request.Sorts.Count == 0)//默认按Id倒序排列
                 request.Sorts.Add(new SortDescriptor { Member = "GameId", SortDirection = ListSortDirection.Descending });
 
-            return Json(lolService.GetAllBattles().Select(b=>new{b.BattleType ,b.Duration,b.GameId ,b.Id ,b.StartTime,b.ChampionId,b.IsWin  }).ToDataSourceResult(request));
+            return Json(lolService.GetAllBattles().Select(b => new { b.BattleType, b.Duration, b.GameId, b.Id, b.StartTime, b.ChampionId, b.IsWin }).ToDataSourceResult(request));
         }
 
         public ActionResult ShowChampionInfo()
         {
-           var recodes= lolService.GetAllRecords().Where(l=>l.Battle.BattleType ==6).Select(l=>new{l.ChampionId,l.IsWin,l.Name}).ToList();
+            var recodes = lolService.GetAllRecords().Where(l => l.Battle.BattleType == 6).Select(l => new { l.ChampionId, l.IsWin, l.Name }).ToList();
             var champions = from r in recodes
-                group r by r.ChampionId
-                into g
-                select g.Key;
+                            group r by r.ChampionId
+                            into g
+                            select g.Key;
             List<LolChampionInfoViewMode> model = new List<LolChampionInfoViewMode>();
             foreach (var c in champions)
             {
-              var heroRecords=  recodes.Where(a => a.ChampionId == c).ToList();
+                var heroRecords = recodes.Where(a => a.ChampionId == c).ToList();
                 model.Add(new LolChampionInfoViewMode
                 {
-                    ChampionId =c,
-                    TotalApprance =heroRecords.Count ,
-                    TotalWinCount =heroRecords.Where(a=>a.IsWin ==1).Count(),
-                    MyApprance =heroRecords.Where (a=>a.Name =="网络中断突然"||a.Name =="笨笨秒杀上帝").Count(),
-                    MyWinCount = heroRecords.Where(a =>a.Name == "网络中断突然" || a.Name == "笨笨秒杀上帝").Count(a=>a.IsWin ==1)
+                    ChampionId = c,
+                    TotalApprance = heroRecords.Count,
+                    TotalWinCount = heroRecords.Where(a => a.IsWin == 1).Count(),
+                    MyApprance = heroRecords.Where(a => MyHeroList.Contains(a.Name)).Count(),
+                    MyWinCount = heroRecords.Where(a => MyHeroList.Contains(a.Name)).Count(a => a.IsWin == 1)
                 });
             }
-            return View(model.OrderByDescending(l=>l.MyApprance).ToList());
+            return View(model.OrderByDescending(l => l.MyApprance).ToList());
         }
         public ActionResult WaitUpdate()
         {
-           var Ids= lolService.GetUpdateIds();
+            var Ids = lolService.GetUpdateIds();
             return Json(Ids);
         }
         public ActionResult UpdateBattle(string ids)
         {
-           var Ids= ids.Split(',');
+            var Ids = ids.Split(',');
             List<int> gameIds = new List<int>();
             foreach (var id in Ids)
             {
@@ -77,8 +79,30 @@ namespace Fzrain.Web.Controllers
         }
         public ActionResult GetBattleDetail(int gameId)
         {
-           var records= lolService.GetAllRecords().Where(r => r.Battle.GameId == gameId).ToList();
-            return PartialView("BattleDetail",records);
+            var records = lolService.GetAllRecords().Where(r => r.Battle.GameId == gameId).ToList();
+            return PartialView("BattleDetail", records);
+        }
+
+        public ActionResult MyPerformance(int? championId)
+        {
+            championId = championId ?? 1;
+            var records = lolService.GetAllRecords().Where(r => r.ChampionId == championId);
+           double avg= records.Average(r => r.Kill);
+            ViewBag.Avg = new List<double> { avg , avg , avg , avg , avg , avg , avg , avg , avg , avg };
+            ViewBag.AvgRecord = "平均击杀：" +
+                                records.Where(r => MyHeroList.Contains(r.Name)).Average(r => r.Kill).ToString("0.00") +
+                                " 平均死亡：" +
+                                records.Where(r => MyHeroList.Contains(r.Name)).Average(r => r.Death).ToString("0.00") +
+                                " 平均助攻：" +
+                                records.Where(r => MyHeroList.Contains(r.Name)).Average(r => r.Assist).ToString("0.00");
+
+            return View(records.Where(r => MyHeroList.Contains(r.Name)).OrderByDescending(r => r.Battle.StartTime).Take(10).ToList());
+        }
+
+        public ActionResult FilterMenuChampion()
+        {
+
+            return Json(lolService.GetAllRecords().GroupBy(r => r.ChampionId).Select(c => c.Key), JsonRequestBehavior.AllowGet);
         }
     }
 }
